f2b3c2625b4c1fa2d830ba4e8c464e4bbd897acb
hilmi <hilmi.kale@ogr.iu.edu.tr>
2024-07-08 17:45:31 +0300

    chore: Add use-debounce library for search input debounce

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -2,14 +2,16 @@
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import { useSearchParams, useRouter, usePathname } from 'next/navigation';
+import { useDebouncedCallback } from 'use-debounce';
 
 export default function Search({ placeholder }: { placeholder: string }) {
   const searchParams = useSearchParams();
   const { replace } = useRouter();
   const pathName = usePathname();
 
-  const handleSearch = (term: string) => {
+  const handleSearch = useDebouncedCallback((term) => {
     console.log(`Searching... ${term}`);
+
     const params = new URLSearchParams(searchParams);
     if (term) {
       params.set('query', term);
@@ -17,9 +19,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
       params.delete('query');
     }
     replace(`${pathName}?${params.toString()}`);
-    console.log('searchParams', searchParams);
-    console.log('params', params);
-  };
+  }, 300);
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
