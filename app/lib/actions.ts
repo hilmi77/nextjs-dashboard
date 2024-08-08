676a0c579588441ba66887c845fa9d79d79c6cd2
hilmi <hilmi.kale@ogr.iu.edu.tr>
2024-08-08 14:04:10 +0300

    feat: Add updateInvoice function to handle invoice updates

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -15,6 +15,8 @@ const FormSchema = z.object({
 
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
 
+const UpdateInvoice = FormSchema.omit({ id: true, date: true });
+
 export async function createInvoice(formData: FormData) {
   const { customer_id, amount, status } = CreateInvoice.parse({
     customer_id: formData.get('customerId'),
@@ -40,4 +42,28 @@ export async function createInvoice(formData: FormData) {
   );
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
+}
+
+export async function updateInvoice(id: string, formData: FormData) {
+  const { customer_id, amount, status } = UpdateInvoice.parse({
+    customer_id: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  });
+
+  const amountInCents = amount * 100;
+
+  await sql`
+    UPDATE invoices
+    SET customer_id = ${customer_id}, amount = ${amountInCents}, status = ${status}
+    WHERE id = ${id}
+  `;
+
+  revalidatePath('/dashboard/invoices');
+  redirect('/dashboard/invoices');
+}
+
+export async function deleteInvoice(id: string) {
+  await sql`DELETE FROM invoices WHERE id = ${id}`;
+  revalidatePath('/dashboard/invoices');
 }
