75fd16e27989ce491f2541c2d6dc63c9221d3a9a
hilmi <hilmi.kale@ogr.iu.edu.tr>
2024-08-15 14:44:37 +0300

    feat: Add lazy loading for images in SideNav component

--- app/ui/dashboard/sidenav.tsx
+++ app/ui/dashboard/sidenav.tsx
@@ -3,6 +3,7 @@ import NavLinks from '@/app/ui/dashboard/nav-links';
 import AcmeLogo from '@/app/ui/acme-logo';
 import { PowerIcon } from '@heroicons/react/24/outline';
 import { signOut } from '@/auth';
+import { redirect } from 'next/navigation';
 export default function SideNav() {
   return (
     <div className="flex h-full flex-col px-3 py-4 md:px-2">
@@ -21,6 +22,7 @@ export default function SideNav() {
           action={async () => {
             'use server';
             await signOut();
+            redirect('/login');
           }}
         >
           <button className="flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3">
